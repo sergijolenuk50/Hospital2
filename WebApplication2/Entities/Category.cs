295549926547c210621df57ada0cefa1391ebff3
sergijolenuk50 <sergijolenuk50@gmail.com>
2024-07-18 18:42:22 +0300

    Hospital2

---
+++ WebApplication2/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // ---- navigation properties
+        public ICollection<Doctor>? Doctors { get; set; } // One to Many
+    }
+}
---
+++ WebApplication2/Entities/Doctor.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication2.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string? ImageUrl { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Work_experience { get; set; }
+        public int CategoryId { get; set; }
+        public bool Archived { get; set; }
+
+        // ---- navigation properties
+        public Category? Category { get; set; }
+
+    }
+}
